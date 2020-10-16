47a1b4410c87fe5c581cc1910c881f8b93e576dd
andrey <wkafan003@gmail.com>
2020-10-17 00:05:22 +0300

    Add log to database

--- configurations/settings.py
+++ configurations/settings.py
@@ -1,11 +1,22 @@
+from typing import Dict
+
 TOKEN = "TOKEN"
 NAME = "miami_bot"
 WEBHOOK = False
-## The following configuration is only needed if you setted WEBHOOK to True ##
+# Db configuration
+DB_OPTIONS: Dict[str, str] = {
+    'dbname': 'postgres',
+    'user': 'postgres',
+    'host': '127.0.0.1',
+    'port': '5432',
+    'password': 'PASSWORD',
+}
+## The following configuration is only needed if you setted WEBHOOK to True
 WEBHOOK_OPTIONS = {
     'listen': '0.0.0.0',  # IP
     'port': 443,
     'url_path': TOKEN,  # This is recommended for avoiding random people
-                        # making fake updates to your views
+    # making fake updates to your views
 }
 WEBHOOK_URL = f'https://example.com/{WEBHOOK_OPTIONS["url_path"]}'
+OPW_KEY = 'TOKEN'
